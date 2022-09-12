58184e05aa29189b58be74c5541136aa2f8d5295
Dakaro Mueller <LukarioMC@gmail.com>
2022-09-11 23:00:15 -0700

    Added documentation, changed indentation styling to 2 spaced.

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,18 @@
 package org.comp2522;
 
+/**
+ * An example Java program that outputs a message to the console.
+ *
+ * @author Dakaro Mueller
+ * @version 22.09.11
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("I love video games and computers!");
-    }
+  /**
+   * The main entry point of the program.
+   *
+   * @param args (unused)
+   */
+  public static void main(String[] args) {
+    System.out.println("I love video games and computers!");
+  }
 }
